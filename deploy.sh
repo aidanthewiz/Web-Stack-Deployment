cddbf5c7ac4c7553c6eb8f14d5da9ff057b5986e
Aidan Artherton <Aidanartherton@gmail.com>
2019-07-03 02:36:18 -0400

    Better root check
    Check for root using all possible cases and other basic optimizations.

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 # Web Stack Deployment
 # Copyright (c) 2019, aidanthewiz
 
-# Exit if the script is not being run as root.
-if [[ $EUID -ne 0 ]]; then
+# Exit if the script is not being run as root and output to stderr.
+if [[ $EUID -ne 0 ]] || [[ $(id -u) -ne 0 ]]; then
    echo "This deploy script must be run as root 'sudo ./deploy.sh'" >&2
    exit 1
 fi
 
-# Update and Full Upgrade before configuration.
+# Update and Full Upgrade before configuration
 apt update
 apt full-upgrade -y
 
@@ -18,8 +18,8 @@ echo "Would you like to enable ufw (firewall) and allow OpenSSH?"
 select yn in "Yes" "No"; do
     case $yn in
     Yes)
-        sudo ufw enable
-        sudo ufw allow OpenSSH
+        ufw enable
+        ufw allow OpenSSH
         break
     ;;
     No)
@@ -29,7 +29,7 @@ select yn in "Yes" "No"; do
 done
 
 # Ask to change hostname
-echo "Would you like to change your hostname?"
+echo "Would you like to change your hostname (input will be trimmed for white space)?"
 select yn in "Yes" "No"; do
     case $yn in
     Yes)
