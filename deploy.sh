9555d10761105e35ea35e6603fcce647c928d43a
Aidan <aidanartherton@gmail.com>
2019-12-28 20:25:17 -0500

    Remove unused package

--- deploy.sh
+++ deploy.sh
@@ -53,7 +53,7 @@ lemp () {
     add-apt-repository ppa:certbot/certbot -y
     apt update
     apt full-upgrade -y
-    apt install build-essential nginx certbot checkinstall libpcre3 libpcre3-dev zlib1g-dev unzip uuid-dev libssl-dev libxslt1-dev libxml2-dev libgeoip-dev libgoogle-perftools-dev libperl-dev php7.4-fpm php7.4-common php7.4-cli php7.4-gd php7.4-zip php7.4-mbstring php7.4-xml php7.4-xmlrpc php7.4-soap php7.4-intl php7.4-mysql php7.4-curl composer -y
+    apt install build-essential nginx certbot libpcre3 libpcre3-dev zlib1g-dev unzip uuid-dev libssl-dev libxslt1-dev libxml2-dev libgeoip-dev libgoogle-perftools-dev libperl-dev php7.4-fpm php7.4-common php7.4-cli php7.4-gd php7.4-zip php7.4-mbstring php7.4-xml php7.4-xmlrpc php7.4-soap php7.4-intl php7.4-mysql php7.4-curl composer -y
     apt remove *nginx* -y
     NGINX_VERSION=1.16.1
     wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz --directory-prefix=/usr/local/src/
